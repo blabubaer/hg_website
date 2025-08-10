b58d94bb9eb80f722d08a456fa1a7d469c102267
blabubaer <mgransow@gmail.com>
2025-08-10 19:46:11 +0200

    Update build script to ensure static directories exist

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Create necessary directories
+mkdir -p static/images/team
+mkdir -p static/favicon
+
 python manage.py collectstatic --no-input
 python manage.py migrate 
